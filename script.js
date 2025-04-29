dbfa19f942090755b79a38fe84c920c030d6853f
aima kareem <amikareem921@gmail.com>
2025-04-29 19:24:31 +0500

    calculator

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const display = document.getElementById('display');  
 const buttons = document.querySelectorAll('button');  
@@ -23,32 +22,3 @@ buttons.forEach(btn => {
     });
 });
 
-
-=======
-
-const display = document.getElementById('display');  
-const buttons = document.querySelectorAll('.btn');  
-buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        const value = btn.textContent;  
-
-        if (value === 'C') {
-            display.value = "";  
-        } else if (value === '=') {
-            try {
-                
-                display.value = Function('return ' + display.value)();
-            } catch (e) {
-                display.value = 'Error'; 
-            }
-        } else {
-            display.value += value;  
-        }
-    });
-});
-
-
-
-
-
->>>>>>> 5ce3f1b058587dd91d63cb5c27357df184484b8c
